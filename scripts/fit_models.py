149bf7f60b4cab13b419d93b6f851a96710edcd2
mlevis1 <michael.levis@colorado.edu>
2019-04-19 11:29:56 -0600

    update repo

--- scripts/fit_models.py
+++ scripts/fit_models.py
@@ -1,4 +1,16 @@
-"""A module to read images and fit models"""
+"""A module to read images, fit models, and print confusion matrices"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from glob import glob
+import cv2
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn import linear_model
+from sklearn import svm
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
 
 def read_images(paths):
     """
@@ -10,12 +22,11 @@ def read_images(paths):
 
     Returns
     ----------
-    images and list of id numbers
+    data : list of image id numbers
     """
-    import numpy as np
-    #Get list of images
-    images = (glob(paths + '*.jpg'))
-    #Read images from list
+    # Get list of images
+    images = glob(paths + '*.jpg')
+    # Read images from list
     data = [cv2.cvtColor(cv2.imread(file),cv2.COLOR_BGR2GRAY) for file in images]
     data = np.array(data)
     data = data.reshape((len(data),-1))
@@ -30,10 +41,13 @@ def svm_layers(X_train, y_train, X_test, y_test):
 
     Parameters
     ------------
-    X_train = array
-    X_test = array
-    y_train = data frame or array
-    y_test = data frame or array
+    X_train : array containing attributes for training data
+
+    y_train : array containing labels for training data
+
+    X_test : array containing attributes for testing data
+
+    y_test : array containing labels for testing data
 
     Returns
     ------------
@@ -46,17 +60,24 @@ def svm_layers(X_train, y_train, X_test, y_test):
     print(classification_report(y_test,y_pred))
     accuracy = accuracy_score(y_test,y_pred)
 
-    return model,accuracy
+    return model, accuracy, y_pred, X_test
 
-def supervised_models(model, X_train, y_train):
+def supervised_models(model, X_train, y_train, X_test, y_test):
     """
     Fits supervised models to data and returns metrics
 
     Parameters
     ------------
-    model = supervised learning model
-    X_train = array
-    y_train = data frame or array
+    model : supervised learning model
+        linear regression, random forest, gradient boosting
+
+    X_train : array containing attributes for training data
+
+    y_train : array containing labels for training data
+
+    X_test : array containing attributes for testing data
+
+    y_test : array containing labels for testing data
 
     Returns
     ------------
@@ -71,3 +92,49 @@ def supervised_models(model, X_train, y_train):
     print(classification_report(y_test,y_pred))
 
     return model, probabilities, y_pred
+
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("")
+    else:
+        print('')
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # Show all ticks
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # Label ticks with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate tick labels and set alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
